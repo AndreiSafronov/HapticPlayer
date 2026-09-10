88c91c7927b8a613f4356f1049f11cee4ef89b7f
Andrew <denxizofficial@gmail.com>
2026-09-10 18:44:55 +0300

    Refactor HapticClip initialization in ViewController
    Removed hardcoded array of HapticClip instances and updated the SwiftUI view initialization.

--- HapticPlayer/ViewController.swift
+++ HapticPlayer/ViewController.swift
@@ -1,59 +1,14 @@
-//
-//  ViewController.swift
-//  HapticPlayer
-//
-//  Created by Thomas Dye on 26/08/2025.
-//
-
 import UIKit
-
 import SwiftUI
 
 class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
-        // Build your array here (same as in Preview)
-
-
-        let clips = [
-            HapticClip(
-                title: "Apple F1 Trailer",
-                subtitle: "Remote AHAP",
-                videoURL: .remoteURL(URL(string: "https://play-edge.itunes.apple.com/WebObjects/MZPlayLocal.woa/hls/subscription/playlist.m3u8?cc=GB&svcId=tvs.vds.4041&a=1809419955&isExternal=true&brandId=tvs.sbd.4000&id=1134594683&l=en-GB&aec=UHD")!),
-                haptic: .remoteURL(URL(string: "https://vod-ak-svod.tv.apple.com/itunes-assets/HLSVideo211/v4/39/ab/de/39abde35-7e15-877a-4bc5-885724efdeb7/apx_touch_clip_evergreen.ahap")!),
-                thumbSystemImage: "bolt.fill"
-            ),
-            HapticClip(
-                title: "My Haptic Video",
-                subtitle: "Bundled AHAP",
-                videoURL: .bundled(name: "00BA71CD-2C54-415A-A68A-8358E677D750"),
-                haptic: .bundled(name: "test"),
-                thumbSystemImage: "waveform.path.badge.plus"
-            ),
-            HapticClip(
-                title: "Explosion FX",
-                subtitle: "Strong Impact",
-                videoURL: .remoteURL(URL(string: "https://example.com/video/explosion.m3u8")!),
-                haptic: .remoteURL(URL(string: "https://example.com/haptics/explosion.ahap")!),
-                thumbSystemImage: "flame.fill"
-            ),
-            HapticClip(
-                title: "Sci-Fi Pulse",
-                subtitle: "Rhythmic Pattern",
-                videoURL: .bundled(name: "SampleVideo2"),
-                haptic: .bundled(name: "pulse"),
-                thumbSystemImage: "antenna.radiowaves.left.and.right"
-            )
-        ]
-
-
-        // Present SwiftUI
-        let swiftUIView = HapticClipGridView(clips: clips)
+        // Launch the liquid glass SwiftUI view directly
+        let swiftUIView = HapticClipGridView()
         let host = UIHostingController(rootView: swiftUIView)
         host.modalPresentationStyle = .fullScreen
         present(host, animated: true)
     }
 }
-
-
